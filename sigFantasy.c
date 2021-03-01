e250060411a8c5594c457af5ed6fefa0627c08b4
Leonardo Dantas <leonardodds@hotmail.com>
2021-03-01 00:25:40 +0000

    Projeto 5/15

--- sigFantasy.c
+++ sigFantasy.c
@@ -61,30 +61,7 @@ int main(void) {
 						break;
 		} 		
 	} while (opcao != '0');
-	return 0;
-    
-    
-    //menuSobre();
-    //opcao = menuPrincipal();
-    //opcao = menuCliente();
-    //opcao = menuFantasia();
-    //opcao = menuAlugueis();
-    //opcao = menuRelatorios();
-    //telaCadastrarCliente();
-    //telaPesquisarCliente();
-    //telaAtualizarCliente();
-    //telaExcluirCliente();   
-    //telaCadastrarFantasia();
-    //telaPesquisarFantasia();
-    //telaAtualizarFantasia();
-    //telaExcluirFantasia();
-    //telaAlugarFantasia();
-    //telaDevolverFantasia();
-    //telaListarFantasia();
-    //telaHistoricoFantasia();
-    //telaVerificarDatas();
-    //telaClienteFiel();
-    //telaFantasiaMaisAlugada(); 
+	return 0; 
 
 }
 
@@ -128,45 +105,4 @@ char menuPrincipal(void) {
 
 }
 
-void menuSobre(void) {
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-    printf("///               Departamento de Computação e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///             Projeto Sistema de Aluguéis de Roupas e Fantasias           ///\n");
-    printf("///            Developed by  @PedroAugustoMD and @leonardodantas4           ///\n");
-    printf("///                             Jan, 2021                                   ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///           = = = = = Sistema de Aluguel de Fantasias = = = = =           ///\n");
-    printf("///                                                                         ///\n");
-    printf("///  Projeto desenvolvido para a disciplina DCT1106 - Programação, com o    ///\n");
-    printf("///  objetivo de alugar fantasias para os usuários. Esse projeto contém as  ///\n");
-    printf("///  funcionalidades principais de uma loja de fantasias que serão          ///\n");
-    printf("///  escolhidas pelos usuários do programa. Essas funcionalidades são:      ///\n");
-    printf("///  cadastro, consulta, alteração, exclusão, aluguel, devolução, listas    ///\n");
-    printf("///  de fantasias disponíveis, histórico de aluguéis, relatório, cliente    ///\n");
-    printf("///  mais fiel e fantasia mais alugada.                                     ///\n");
-    printf("///                                                                         ///\n");
-    printf("///  Bem vindos a SIG-Fantasy, o melhor local para alugar fantasias e       ///\n");
-    printf("///  roupas do Brasil, aqui você poderá alugar os seus looks pelo melhor    ///\n");
-    printf("///  preço do mercado. Está indo para uma festa fantasia e ainda não sabe o ///\n");
-    printf("///  que vestir? Veja as fantasias disponíveis e se divirta como nunca!     ///\n");
-    printf("///                                                                         ///\n");
-    printf("///  Atenção: em caso de atrasos, a SIG-Fantasy cobra R$2,00 a cada dia     ///\n");
-    printf("///  pendente!                                                              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-	  printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	  getchar();
-}
 
-
-void moduloInformacoes(void) {
-	menuSobre();
-}
