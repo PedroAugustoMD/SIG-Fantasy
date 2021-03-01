b6ea8f43eb1dfbde6bdf0432c61ee2a87fd7130a
Leonardo Dantas <leonardodds@hotmail.com>
2021-03-01 00:13:56 +0000

    Projeto 5/15

--- sigFantasy.c
+++ sigFantasy.c
@@ -167,44 +167,6 @@ void menuSobre(void) {
 }
 
 
-char menuCliente(void) {
-  char op;
-  system("clear");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          = = = =             Sig Fantasy             = = = =          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///                            Developed by                               ///\n");
-  printf("///            @PedroAugustoMD & @leonardodantas4 - Jan, 2021             ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = = = = = = Menu Cliente = = = = = = = = =            ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           1. Cadastrar um novo cliente                                ///\n");
-	printf("///           2. Pesquisar os dados de um cliente                         ///\n");
-	printf("///           3. Atualizar o cadastro de um cliente                       ///\n");
-	printf("///           4. Excluir um cliente do sistema                            ///\n");
-	printf("///           0. Voltar ao menu anterior                                  ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           Escolha a opção desejada: ");
-  scanf("%c", &op);
-  getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-  printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
-  return op;
-
-}
 
 char menuFantasia(void) {
   char op;
@@ -320,144 +282,7 @@ char menuRelatorios(void) {
   return op;
 }
 
-void telaCadastrarCliente(void) {
-  char cpf[12];
-	char nome[52];
-	char email[52];
-	char telefone[13];
-  system("clear");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          = = = =             Sig Fantasy             = = = =          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///                            Developed by                               ///\n");
-  printf("///            @PedroAugustoMD & @leonardodantas4 - Jan, 2021             ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =           ///\n");
-	printf("///           = = = = = = = = Cadastrar Cliente = = = = = = = =           ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =           ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           CPF (Apenas números!): ");
-  scanf("%[0-9]", cpf);
-  getchar();
-	printf("///           Nome: ");
-  scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
-  getchar();
-	printf("///           E-mail: ");
-  scanf("%[a-z@.]", email);
-  getchar();
-	printf("///           Telefone (Apenas números!): ");
-  scanf("%[0-9]", telefone);
-  getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
-}
 
-void telaPesquisarCliente(void) {
-  char cpf[12];
-  system("clear");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          = = = =             Sig Fantasy             = = = =          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///                            Developed by                               ///\n");
-  printf("///            @PedroAugustoMD & @leonardodantas4 - Jan, 2021             ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =           ///\n");
-	printf("///           = = = = = = = = Pesquisar Cliente = = = = = = = =           ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =           ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           CPF: ");
-  scanf("%[0-9]", cpf);
-  getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
-}
-
-void telaAtualizarCliente(void) {
-    char cpf[12];
-    char item[9];
-    system("clear");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          = = = =             Sig Fantasy             = = = =          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///                            Developed by                               ///\n");
-  printf("///            @PedroAugustoMD & @leonardodantas4 - Jan, 2021             ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =           ///\n");
-	printf("///           = = = = = = = = Atualizar Cliente = = = = = = = =           ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = = =           ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           Digite o CPF do cliente: ");
-  scanf("%[0-9]", cpf);
-  getchar();
-	printf("///           Digite o item a ser atualizado: ");
-  scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", item);
-  getchar();
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
-}
-
-void telaExcluirCliente(void) {
-      char cpf[12];
-    system("clear");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          = = = =             Sig Fantasy             = = = =          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///                            Developed by                               ///\n");
-  printf("///            @PedroAugustoMD & @leonardodantas4 - Jan, 2021             ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = = = = = Excluir Cliente = = = = = = = =             ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           CPF: ");
-  scanf("%[0-9]", cpf);
-  getchar();
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
-}
 
 void telaCadastrarFantasia(void) {
     char id[12];
@@ -808,26 +633,6 @@ void telaFantasiaMaisAlugada(void) {
 	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
 }
-
-void moduloCliente(void) {
-  char opcao;
-  do {
-		opcao = menuCliente();
-		switch(opcao) {
-			case '1': 	telaCadastrarCliente();
-						break;
-			case '2': 	telaPesquisarCliente();
-						break;
-			case '3': 	telaAtualizarCliente();
-						break;
-			case '4': 	telaExcluirCliente();
-						break;
-		} 		
-	} while (opcao != '0');
-
-}
-
-
 
 void moduloFantasia(void) {
 	char opcao;
